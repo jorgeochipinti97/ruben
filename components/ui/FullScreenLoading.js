b2241c1e8b5c76ac2dbbce7e52f715d2434727c2
Jorge Ochipinti <jorgeochipinti@Air-de-Jorge.fibertel.com.ar>
2022-06-18 18:05:02 -0300

    ad

--- components/ui/FullScreenLoading.js
+++ components/ui/FullScreenLoading.js
@@ -12,7 +12,7 @@ export const FullScreenLoading = () => {
         height='calc(100vh - 200px)'
         width={'100vw'}
     >
-        <Typography sx={{ mb: 3 }} variant="h2"  fontWeight={ 200 } fontSize={ 50 }>Espere por favor...</Typography>
+        <Typography sx={{ mb: 3, fontSize:{xs:20,md:50,lg:50,xl:40} }} variant="h2"  fontWeight={ 200 } >Espere por favor...</Typography>
         <CircularProgress thickness={ 2 } />
     </Box>
   )
