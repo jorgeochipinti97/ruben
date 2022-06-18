a17fbbc469c126c15ae66855b1d840461c9c1e4b
Jorge Ochipinti <jorgeochipinti@Air-de-Jorge.fibertel.com.ar>
2022-06-18 18:00:14 -0300

    sss

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
       clearInterval(int)
     }, 2000);
   }, [])
-  const pics = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+  const pics = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,36,37,38,39]
 
   return (
     <>
@@ -39,7 +39,7 @@ export default function Home() {
               <Grid item xs={12} md={6} lg={6} xl={3} key={`${e}`}>
                 <div data-aos="flip-right" data-aos-delay="300">
                   <Box display='flex' justifyContent='center' sx={{ m: 3 }}>
-                    <Image src={e <= 29 ? `/${e}.jpeg` : `/${e}.png`} width={480} height={420} />
+                    <Image src={e <= 29  || e == 36 || e == 37 || e == 38 || e == 39 ? `/${e}.jpeg` : `/${e}.png`} width={1150} height={1000} />
                   </Box>
                 </div>
               </Grid>
