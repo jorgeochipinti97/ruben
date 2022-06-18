7ddbaab4a84a86f83982f734713419cbf1ed4553
Jorge Ochipinti <jorgeochipinti@MacBook-Air-de-Jorge.local>
2022-06-18 11:03:12 -0300

    fix

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
       clearInterval(int)
     }, 2000);
   }, [])
-  const pics = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,30,31,32,33,34,35]
+  const pics = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
 
   return (
     <>
@@ -32,14 +32,14 @@ export default function Home() {
         <Grid container>
           {!time
             ? (
-                <FullScreenLoading />
+              <FullScreenLoading />
 
             )
             : pics.map(e => (
-              <Grid item xs={12} md={6} lg={6} xl={3}>
-                <div data-aos="flip-right"   data-aos-delay="300">
+              <Grid item xs={12} md={6} lg={6} xl={3} key={`${e}`}>
+                <div data-aos="flip-right" data-aos-delay="300">
                   <Box display='flex' justifyContent='center' sx={{ m: 3 }}>
-                    <Image src={e <=29 ? `/${e}.jpeg` : `/${e}.png`} width={480} height={420} />
+                    <Image src={e <= 29 ? `/${e}.jpeg` : `/${e}.png`} width={480} height={420} />
                   </Box>
                 </div>
               </Grid>
